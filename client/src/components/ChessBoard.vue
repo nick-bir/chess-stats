5a5dff2de2f808d3c325e373f3e2e5e714dc9227
off-border <off-border-github@alpah.ru>
2020-12-05 14:18:53 +0200

    added default board setup

--- client/src/components/ChessBoard.vue
+++ client/src/components/ChessBoard.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
     div.chess-board
         div.chess-board__content
-            .chess-board__row(v-for="r in 8")
-                .chess-board__tile(v-for="c in 8" :class="'chess-board__tile_side_' + ((r+c)%2 ? 'dark' : 'light')")
-                    .chess-board__tile-content
-                        //- |{{r}}:{{c}}
-                        ChessFigure
+            .chess-board__letters
+                .chess-board__letter(v-for="l in 'abcdedgh'") {{ l }}
+            .chess-board__digits
+                .chess-board__digit(v-for="l in '87654321'") {{ l }}
+            
+            .chess-board__tiles
+                .chess-board__row(v-for="(d, r) in '87654321'")
+                    .chess-board__tile(v-for="(l, c) in 'abcdedgh'" :class="'chess-board__tile_side_' + ((r+c)%2 ? 'dark' : 'light')")
+                        .chess-board__tile-content
+                            //- |{{r}}:{{c}}
+                            ChessFigure(:figure="boardSetup[l+d]" v-if="boardSetup[l+d]")
                 
+            .chess-board__digits
+                .chess-board__digit(v-for="l in '87654321'") {{ l }}
+            .chess-board__letters
+                .chess-board__letter(v-for="l in 'abcdedgh'") {{ l }}
 
     </div>
 </template>
@@ -15,12 +25,54 @@
 import ChessFigure from './ChessFigure'
 export default {
     name: 'ChessBoard',
+    created() {
+        console.log('----chessBoard created');
+    },
+    data() {
+        return {
+            boardSetup: {
+                'a1': 'rl',
+                'b1': 'nl',
+                'c1': 'bl',
+                'd1': 'ql',
+                'e1': 'kl',
+                'f1': 'bl',
+                'g1': 'nl',
+                'h1': 'rl',
+
+                'a2': 'pl',
+                'b2': 'pl',
+                'c2': 'pl',
+                'd2': 'pl',
+                'e2': 'pl',
+                'f2': 'pl',
+                'g2': 'pl',
+                'h2': 'pl',
+
+                
+                'b8': 'nd',
+                'a8': 'rd',
+                'c8': 'bd',
+                'd8': 'qd',
+                'e8': 'kd',
+                'f8': 'bd',
+                'g8': 'nd',
+                'h8': 'rd',
+                'a7': 'pd',
+                'b7': 'pd',
+                'c7': 'pd',
+                'd7': 'pd',
+                'e7': 'pd',
+                'f7': 'pd',
+                'g7': 'pd',
+                'h7': 'pd',
+            }
+        }
+    },
     components: {
         ChessFigure
     },
-    created() {
-        console.log('----chessBoard created');
-    }
+    
 }
 </script>
 
@@ -29,7 +81,6 @@ export default {
         border: 1px solid silver;
 
     responsive-square() 
-        dev-border()
         position: relative
         &:after
             content: ''
@@ -46,8 +97,46 @@ export default {
         margin: auto
         border: 1px solid silver
 
+        &__content
+            display: flex
+            flex-wrap: wrap
+
+        &__letters
+        &__digits
+        &__tiles
+            box-sizing: border-box
+
+        &__letters
+            width: 100%
+            height: 5%
+            margin: 0 5%;
+            display: flex
+
+        &__letter
+            flex-grow: 1
+            border: 0px solid silver
+            border-right-width: 1px
+            &:first-child
+                border-left-width: 1px
+
+        &__digits
+            width: 5%
+            height: 90%
+            display: flex
+            flex-direction: column
+
+        &__digit
+            flex-grow: 1
+            border: 0px solid silver
+            border-bottom-width: 1px
+            &:first-child
+                border-top-width: 1px
+
+        &__tiles
+            width: 90%
+            height: 90%
+
         &__row
-            dev-border()
             box-sizing: border-box;
             width: 100%
             height: 12.5%
--- client/src/components/ChessFigure.vue
+++ client/src/components/ChessFigure.vue
@@ -1,15 +1,16 @@
 <template>
-  <svg class="ChessFigure">
+  <!-- <svg class="chess-figure" viewbox="0 0 45 45">
     <use :xlink:href="svg+'#chess-figure__icon-klt'"></use>
-  </svg>
+  </svg> -->
+  <div class="chess-figure" :class="`chess-figure_figure_${figure}`"></div>
 </template>
 
 <script>
 
 export default {
-  data(){
-    return {
-      svg: require('./ChessFigure__Sprite.svg')
+  props: {
+    figure: {
+      default: 'ql'
     }
   }
   
@@ -17,8 +18,35 @@ export default {
 </script>
 
 <style lang="stylus">
-  .ChessFigure
+  .chess-figure
     width: 100%
     height: 100%
+    background-image: url('ChessFigure__Sprite.svg')
+    background-size: 600% 200%
 
+    &_figure_kl
+      background-position: 0 0
+    &_figure_ql
+      background-position: 20% 0
+    &_figure_rl
+      background-position: 40% 0
+    &_figure_bl
+      background-position: 60% 0
+    &_figure_nl
+      background-position: 80% 0
+    &_figure_pl
+      background-position: 100% 0
+
+    &_figure_kd
+      background-position: 0 100%
+    &_figure_qd
+      background-position: 20% 100%
+    &_figure_rd
+      background-position: 40% 100%
+    &_figure_bd
+      background-position: 60% 100%
+    &_figure_nd
+      background-position: 80% 100%
+    &_figure_pd
+      background-position: 100% 100%
 </style>
