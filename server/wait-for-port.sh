791ec29c0373fdca982c4449dc76c0b02a218bab
off-border <off-border-github@alpah.ru>
2020-12-05 12:38:33 +0200

    api: пофиксил ожидание mysql

--- server/wait-for-port.sh
+++ server/wait-for-port.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "waiting for $1 to open port $2..."
-while ! timeout 1 bash -c "echo > /dev/tcp/$1/$2";  do  
+# while ! timeout 1 sh -c "echo > /dev/tcp/$1/$2";  do  
+while ! timeout 1 sh -c "nc -vz $1 $2";  do  
   echo waiting...; 
   sleep 3; 
 done
