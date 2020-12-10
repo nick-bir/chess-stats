d9ec45c25fe5e082b605e25c9d4f26f6cdbb8b07
off-border <off-border-github@alpah.ru>
2020-12-10 11:46:24 +0200

    set up Jest and example test file

---
+++ client/src/components/MainLayout.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import MainLayout from '@/components/MainLayout.vue'
+
+describe('App.vue', () => {
+  it('renders chess board', () => {
+    const wrapper = shallowMount(MainLayout, {
+      propsData: {  }
+    })
+    expect(wrapper.find('.chess-board').exists());
+  })
+})
