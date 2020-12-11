1e5c7367033a55ebc4622aa83d1568fafc95020d
off-border <off-border-github@alpah.ru>
2020-12-11 16:16:56 +0200

    workin on getting data from api

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,6 +1,6 @@
-async function loadStats() {
-    await fetch('http://localhost:8081/api/v1/games/stats');
-
+async function loadStats({ commit }) {
+    let res = await (await fetch('http://localhost:8081/api/v1/games/stats')).json();
+    commit('SET_STATS', res);
 }
 
 export default {
