92a42760782552605caeee366066373b02af5021
off-border <off-border-github@alpah.ru>
2021-01-14 19:24:55 +0200

    client: winner filters in progress

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,13 +1,33 @@
 import fetch from 'node-fetch';
 
-async function loadStats({ commit }) {
-    const res = await (await fetch('http://localhost:8081/api/v1/games/stats')).json();
+async function loadStats({ commit, state }) {
+    let url = new URL('http://localhost:8081/api/v1/games/stats');
+    let params = {};
+    let winner = state.filters.winner || {};
+    params.filter = '';
+    if (winner.name)
+        params.filter += `white="${winner.name}" or black="${winner.name}"`;
+    if (winner.side) {
+        params.filter +=
+            (params.filter.length ? ' and ' : '') +
+            `result=${winner.side === 'white' ? 0 : 1}`;
+    }
+
+    if (params.filter.length) {
+        params.filter = `'${params.filter}'`;
+    } else {
+        delete params.filter;
+    }
+
+    url.search = new URLSearchParams(params).toString();
+
+    const res = await (await fetch(url.toString())).json();
     commit('setStats', res);
 }
 
-function toggleFilter({ commit, dispatch }, {filter, value}) {
+function toggleFilter({ commit, dispatch }, { filter, value }) {
     // console.log('---togglefilter', filter, value)
-    commit('toggleFilter', {filter, value});
+    commit('toggleFilter', { filter, value });
 
     if (filter === 'winner') {
         commit('dataRequestStarted');
@@ -15,12 +35,12 @@ function toggleFilter({ commit, dispatch }, {filter, value}) {
     }
 }
 
-function resetFilter({ commit }, {filter}) {
-    commit('resetFilter', {filter});
+function resetFilter({ commit }, { filter }) {
+    commit('resetFilter', { filter });
 }
 
 export default {
     loadStats,
     toggleFilter,
     resetFilter
-}
+};
