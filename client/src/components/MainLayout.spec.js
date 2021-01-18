c9fab99861abc0224d80f23e586d9c904f033ee6
off-border <off-border-github@alpah.ru>
2021-01-18 18:55:44 +0200

    client: fixed tests

--- client/src/components/MainLayout.spec.js
+++ client/src/components/MainLayout.spec.js
@@ -1,12 +1,14 @@
-import { shallowMount } from '@vue/test-utils'
-import MainLayout from '@/components/MainLayout.vue'
+import { shallowMount } from '@vue/test-utils';
+import MainLayout from '@/components/MainLayout.vue';
+import ChessBoard from '@/components/ChessBoard.vue';
+import Filters from '@/components/Filters.vue';
 
 describe('App.vue', () => {
-  it('renders chess board and filters', () => {
-    const wrapper = shallowMount(MainLayout, {
-      propsData: {  }
+    it('renders chess board and filters', () => {
+        const wrapper = shallowMount(MainLayout, {
+            propsData: {}
+        });
+        expect(wrapper.findComponent(ChessBoard).exists()).toBe(true);
+        expect(wrapper.findComponent(Filters).exists()).toBe(true);
     });
-    expect(wrapper.find('.ChessBoard').exists());
-    expect(wrapper.find('.Filters').exists());
-  })
-})
+});
