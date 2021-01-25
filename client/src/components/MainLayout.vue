1e6191b9ae51dfe122717bc2d5bba82e2d61c160
off-border <off-border-github@alpah.ru>
2021-01-25 15:16:49 +0200

    client: LoadingIndicator WIP

--- client/src/components/MainLayout.vue
+++ client/src/components/MainLayout.vue
@@ -1,43 +1,46 @@
 <template>
     <main class="MainLayout">
-        <ChessBoard :stats="stats"/>
+        <ChessBoard :stats="stats" />
         <Filters filters="filters" />
+        <LoadingIndicator v-if="dataRequestStarted"/>
     </main>
 </template>
 
 <script>
 import ChessBoard from './ChessBoard.vue';
 import Filters from './Filters.vue';
+import LoadingIndicator from './LoadingIndicator.vue';
+import { mapGetters } from 'vuex';
 
 export default {
     name: 'MainLayout',
 
     computed: {
-        stats() {
-            return this.$store?.getters.stats;
-        }
+        ...mapGetters(['stats', 'dataRequestStarted'])
     },
 
     components: {
         ChessBoard,
-        Filters
-    }
-}
+        Filters,
+        LoadingIndicator,
+    },
+};
 </script>
 
 <style lang="stylus">
-    .MainLayout
-        display: flex
-        padding: 10px
-        justify-content: center
-        border: 1px solid silver
+.MainLayout {
+    display: flex
+    padding: 10px
+    justify-content: center
+    // border: 1px solid silver
 
-        .ChessBoard
-            width: 50%
-            max-width: 500px
+    .ChessBoard {
+        width: 50%
+        max-width: 500px
+    }
 
-        .Filters
-            margin-left: 20px
-        
-     
+    .Filters {
+        margin-left: 40px
+    }
+}
 </style>
